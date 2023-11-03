b4e7dd45ac142022a9909ac210dcc8c0e1e9c136
josephesteva <joe.esteva@gmail.com>
2023-11-02 22:44:15 -0400

    Build initial page structure and loop for freelancer names

--- script.js
+++ script.js
@@ -9,3 +9,58 @@ const freelancers = [
   { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
 
+const body = document.body;
+const title = document.createElement(`h1`);
+const averageMessage = document.createElement(`p`);
+const subHeading = document.createElement(`h2`);
+const container = document.createElement(`section`);
+const nameBox = document.createElement(`section`);
+const occupationBox = document.createElement(`section`);
+const priceBox = document.createElement(`section`);
+const nameHeading = document.createElement(`h3`);
+const occupationHeading = document.createElement(`h3`);
+const priceHeading = document.createElement(`h3`);
+const nameList = document.createElement(`section`);
+const occupationList = document.createElement(`ul`);
+const priceList = document.createElement(`ul`);
+
+title.textContent = `Freelancer Forum`;
+averageMessage.textContent = `The average starting price is TODO`;
+subHeading.textContent = `Available Freelancers`;
+nameHeading.textContent = `Name`;
+occupationHeading.textContent = `Occupation`;
+priceHeading.textContent = `Starting Price`;
+
+body.appendChild(title);
+body.appendChild(averageMessage);
+body.appendChild(subHeading);
+body.appendChild(container);
+container.appendChild(nameBox)
+container.appendChild(occupationBox)
+container.appendChild(priceBox)
+nameBox.appendChild(nameHeading)
+nameBox.appendChild(nameList)
+occupationBox.appendChild(occupationHeading)
+occupationBox.appendChild(occupationList)
+priceBox.appendChild(priceHeading)
+priceBox.appendChild(priceList)
+
+body.style= `text-align: center`
+container.style = `display: flex; justify-content: space-around`
+
+//iterate through the array to take a specific apecific value for an object and place it in the appropriate list within the website
+
+for (i = 0; i < freelancers.length; i++) {
+	nameList.appendChild(document.createElement(`p`));
+	// console.log(freelancers[i].name);
+	nameList.children[i].textContent = freelancers[i].name;
+}
+
+// freelancers.forEach(element => {
+// 	nameList.appendChild(document.createElement(`li`));
+// 	console.log(element.name)
+// 	console.log(freelancers.name)
+// 	// nameList.children[i].textContent = element.name[i];
+// });
+
+// console.log(freeN)
